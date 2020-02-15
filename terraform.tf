90d43630aa77287889fc66dfc66f3322308ae8d9
Denis Zamolodchikov <d.zamolodchikov@bi.zone>
2020-02-15 22:28:30 +0300

    deploy with ssh key / tf0.12 fixes

--- terraform.tf
+++ terraform.tf
@@ -12,7 +12,7 @@ variable "server_type" {
 
 variable "node_count" {
     type = number
-    default = 1
+    default = 2
 }
 
 variable "os_image" {
@@ -21,15 +21,20 @@ variable "os_image" {
 }
 
 provider "hcloud" {
-  token = "var.hcloud_token"
+    token = var.hcloud_token
+}
+
+data "hcloud_ssh_key" "dzamolodchikov" {
+    fingerprint = "df:6f:cc:e6:82:32:97:c3:d0:b9:02:3d:70:31:c0:e0"
 }
 
 resource "hcloud_server" "kube-node" {
-    count = "var.node_count"
+    count = var.node_count
     name = "kube-node-${count.index + 1}"
-    image = "var.os_image"
-    server_type = "var.server_type"
-    location = "var.server_location"
+    image = var.os_image
+    server_type = var.server_type
+    location = var.server_location
+    ssh_keys  = [data.hcloud_ssh_key.dzamolodchikov.id]
 }
 
 resource "hcloud_network" "internal-1" {
@@ -38,23 +43,23 @@ resource "hcloud_network" "internal-1" {
 }
 
 resource "hcloud_network_subnet" "k8s-subnet" {
-    network_id = "${hcloud_network.internal-1.id}"
+    network_id = hcloud_network.internal-1.id
     type = "server"
     ip_range = "192.168.1.0/24"
     network_zone = "eu-central"
 }
 
 resource "hcloud_server_network" "kubenetwork" {
-    network_id = "${hcloud_network.internal-1.id}"
-    server_id  = "${element(hcloud_server.kube-node.*.id, count.index)}"
-    ip         = "${cidrhost(hcloud_network_subnet.k8s-subnet.ip_range, count.index + 2)}"
-    count      = "var.node_count"
+    network_id = hcloud_network.internal-1.id
+    server_id  = element(hcloud_server.kube-node.*.id, count.index)
+    ip         = cidrhost(hcloud_network_subnet.k8s-subnet.ip_range, count.index + 1)
+    count      = var.node_count
 }
 
 output "public_ips" {
-    value = "${hcloud_server.kube-node.*.ipv4_address}"
+    value = hcloud_server.kube-node.*.ipv4_address
 }
 
 output "private_ips" {
-    value = "${hcloud_server_network.kubenetwork.*.ip}"
+    value = hcloud_server_network.kubenetwork.*.ip
 }
